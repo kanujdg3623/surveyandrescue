1c65b908796c13fa3d59d82165b82fe196798cb0
Kanuj Das Gupta <47301116+kanujdg3623@users.noreply.github.com>
2020-02-19 22:32:41 +0530

    Add files via upload

---
+++ survey_and_rescue/scripts/scheduler.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import roslib
+import sys
+import rospy
+import cv2
+import numpy as np
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from survey_and_rescue.msg import *
+import json
+from rospy_message_converter import message_converter
+from time import time
+import math
+
+class sr_scheduler():
+	def __init__(self):
+		#rospy.Subscriber('/detection_info',SRInfo,self.detection_callback)	
+		rospy.Subscriber('/serviced_info',SRInfo,self.serviced_callback)
+		rospy.Subscriber('/stats_sr',SRDroneStats,self.stats_callback)
+		self.decision_pub = rospy.Publisher('/decision_info',SRInfo,queue_size=4)
+		self.decided_msg = SRInfo()
+		self.decided_msg_prev=SRInfo()
+		self.decided_msg.location="D3"
+		self.decided_msg.info="BASE"
+		self.beacons={}
+		self.servicing=False
+		self.info=["FOOD","MEDICINE","RESCUE"]
+		self.time=time()
+		self.food=0
+		self.medicine=0
+		self.stat=False
+		
+	def detection_callback(self, msg):
+		pass
+
+	def serviced_callback(self,msg):
+		if msg.location!='D3':
+			self.beacons[msg.location][0]="OFF"
+			self.beacons[msg.location][1]=None
+		if(msg.location==self.decided_msg.location):
+			if self.decided_msg.info=="RESCUE" and msg.info=="SUCCESS":
+				self.decided_msg.location="D3"
+				self.decided_msg.info="BASE"
+				self.decision_pub.publish(self.decided_msg)
+				self.servicing=True
+			else:
+				self.servicing=False
+
+	def shutdown_hook(self):
+		pass
+
+	def stats_callback(self,msg):
+		self.food=msg.foodOnboard
+		self.medicine=msg.medOnboard
+		lit=[msg.currentLit.FOOD,msg.currentLit.MEDICINE,msg.currentLit.RESCUE]
+		for i in range(3):
+			for j in lit[i]:
+				cell=chr(ord(j)/10+64)+str(ord(j)%10)
+				try:
+					if self.beacons[cell][0]!=self.info[i]:
+						self.beacons[cell]=[self.info[i],math.floor(time()-self.time)]
+						self.detection_callback([cell,self.info[i]])
+				except KeyError:
+					self.beacons[cell]=[self.info[i],math.floor(time()-self.time)]
+					self.detection_callback([cell,self.info[i]])
+					self.stat=True
+					
+def main(args):
+	sched = sr_scheduler()
+	rospy.init_node('sr_scheduler', anonymous=False)
+	rospy.on_shutdown(sched.shutdown_hook)	
+	rate = rospy.Rate(30)
+	while not sched.stat:
+		pass
+	while not rospy.is_shutdown():
+		cur_col=ord(sched.decided_msg.location[0])-64
+		cur_row=int(sched.decided_msg.location[1])
+		priority=0
+		for location,info in sched.beacons.items():
+			if info[0]=="OFF" or location==sched.decided_msg.location:
+				continue
+			col=ord(location[0])-64
+			row=int(location[1])
+			timespent=(time()-sched.time-info[1])
+			dist=math.sqrt( (cur_row-row)**2 + (cur_col-col)**2 )
+			if info[0]=="FOOD" and timespent<=25:
+				if priority < (10 * sched.food / (dist*timespent)):
+					sched.decided_msg.location=location
+					sched.decided_msg.info=info[0]
+					priority=10 * sched.food / (dist*timespent)
+											
+			elif info[0]=="MEDICINE" and timespent<=25:
+				if priority < (10 * sched.medicine / (dist*timespent)):
+					sched.decided_msg.location=location
+					sched.decided_msg.info=info[0]
+					priority=10 * sched.medicine / (dist*timespent)
+				
+			elif info[0]=="RESCUE" and timespent<=3:
+				if priority < (50 / (dist*timespent)):
+					sched.decided_msg.location=location
+					sched.decided_msg.info=info[0]
+					priority=50 / (dist*timespent)
+		if priority==0:
+			sched.decided_msg.location="D3"
+			sched.decided_msg.info="BASE"
+		if not sched.servicing:
+			sched.decision_pub.publish(sched.decided_msg)
+			sched.decided_msg_prev.location=sched.decided_msg.location
+			sched.decided_msg_prev.info=sched.decided_msg.info
+			sched.servicing=True
+		else sched.decided_msg.info=="RESCUE" and sched.decided_msg_prev.info not in ["RESCUE","BASE"]:
+			sched.decision_pub.publish(sched.decided_msg)
+			sched.decided_msg_prev.location=sched.decided_msg.location
+			sched.decided_msg_prev.info=sched.decided_msg.info
+			sched.servicing=True
+		rate.sleep()
+
+self.out=[0,0,0]
+self.Iterm=[0,0,0]
+if __name__ == '__main__':
+    main(sys.argv)
